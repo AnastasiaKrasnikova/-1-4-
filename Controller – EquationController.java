54e4e118f97251ba9271374895fd8c005c43df04
AnastasiaKrasnikova <gorypsiss@yandex.ru>
2025-05-29 14:17:33 +0300

    Create Controller – EquationController.java

---
+++ Controller – EquationController.java
@@ -0,0 +1,7 @@
+package controller;
+import model.EquationModel;
+import view.EquationView;
+
+public class EquationController {
+    private final EquationModel model;
+    private final EquationView view;
